8fc631fb40d1702d7e235224fb1aa1c24ec444e7
quyettranvu <79063319+quyettranvu@users.noreply.github.com>
2023-10-08 08:53:46 +0700

    fix codes

--- server/server.js
+++ server/server.js
@@ -82,7 +82,8 @@ mongoose.connect(
   }
 );
 
-const PORT = process.env.PORT || 5000;
+// const PORT = process.env.PORT || 5000;
+const PORT = 5000;
 
 app.listen(PORT, () => {
   console.log(`Server is running on ${PORT}`);
